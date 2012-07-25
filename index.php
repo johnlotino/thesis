922acac3495deb3c07a1570b570ec693c13ccac6
johnlotino <john_lotino@yahoo.com>
2012-07-25 09:49:54 +0800

    1st commit ever

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 // change the following paths if necessary
-$yii=dirname(__FILE__).'/../../../School/Yii/yii-1.1.10.r3566/framework/yii.php';
+$yii='C:/wamp/www/yii-1.1.10.r3566/framework/yiic.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 
 // remove the following lines when in production mode
